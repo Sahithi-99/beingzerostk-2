0c502843ea6efd5df4f433e4814945991d1809a1
Sahithi-99 <sahithinarina99@gmail.com>
2020-01-02 17:12:33 +0530

    new API

--- src/app/pricing/pricing.component.ts
+++ src/app/pricing/pricing.component.ts
@@ -7,12 +7,15 @@ import { DataService } from '../data.service';
 })
 export class PricingComponent implements OnInit {
   users ={};
+  count=0;
   constructor(private data:DataService) { }
 
   ngOnInit() {
     this.data.getData().subscribe(d => {
         this.users['arr'] = d;
         alert(this.users.arr.length);
+        this.count=this.users.arr.length;
+
       })
   }
 
