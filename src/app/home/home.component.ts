19a0fa96287956941dc4ec048d4bf0ec8b2cc13f
Sahithi-99 <sahithinarina99@gmail.com>
2020-01-02 16:49:03 +0530

    data_service

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  x:string ="Random data";
 
   constructor() { }
 
   ngOnInit() {
+  }
+  fnclick()
+  {
+    this.x = this.x + " new";
   }
 
 }
--- src/app/pricing/pricing.component.ts
+++ src/app/pricing/pricing.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
+import { DataService } from '../data.service';
 @Component({
   selector: 'app-pricing',
   templateUrl: './pricing.component.html',
   styleUrls: ['./pricing.component.css']
 })
 export class PricingComponent implements OnInit {
-
-  constructor() { }
+  users ={};
+  constructor(private data:DataService) { }
 
   ngOnInit() {
+    this.data.getData().subscribe(d => {
+        this.users['arr'] = d;
+        alert(this.users.arr.length);
+      })
   }
 
 }
